5d9fa9538b3a5215c8ebfc3dadcb6eb715713892
Akif Dere <dereakif@gmail.com>
2021-03-29 10:50:34 +0300

    get residents array

--- src/components/Pages/LocationDetails/LocationDetails.js
+++ src/components/Pages/LocationDetails/LocationDetails.js
@@ -1,8 +1,15 @@
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router";
+import { getCharacter } from "rickmortyapi";
 
 const LocationDetails = () => {
   const location = useLocation();
-  console.log(location.state);
+  useEffect(() => {
+    const residents = getCharacter(
+      location.state.residents.map((item) => item.replace(/\D/g, ""))
+    );
+    console.log(residents);
+  }, []);
   return <div></div>;
 };
 export default LocationDetails;
